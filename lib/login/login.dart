8b69af5851491f8e8cdb3a3a922cfebe704e6d32
nahyunuk <s24022@gsm.hs.kr>
2024-12-12 18:02:35 +0900

    feat: 아이디 및 비밀번호 검사 수정 및 보안

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,4 +1,7 @@
-import 'package:final_test/login/login_assets.dart';
+import 'package:final_test/data/colorData.dart';
+
+import '../data/assets.dart';
+import 'package:final_test/signUp/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -13,8 +16,13 @@ class _LoginState extends State<Login> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  bool _emailHasError = false;
-  bool _passwordHasError = false;
+
+  // 오류 상태 관리
+  bool _hasError = false; // 이메일 또는 비밀번호 오류 여부
+
+  // 테스트용 고정 데이터 (실제 서버 통신 필요시 이 부분을 대체)
+  final String _testEmail = "test@example.com";
+  final String _testPassword = "password123";
 
   @override
   Widget build(BuildContext context) {
@@ -29,21 +37,18 @@ class _LoginState extends State<Login> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SvgPicture.asset(LoginAssets.logo1),
+                  SvgPicture.asset(
+                    Assets.logo1,
+                    placeholderBuilder: (context) =>
+                        const CircularProgressIndicator(),
+                  ),
                   const SizedBox(height: 20),
+                  const SizedBox(height: 10),
                   _buildTextField(
                     controller: _emailController,
                     label: '이메일',
                     hintText: '이메일을 입력해주세요...',
-                    errorState: _emailHasError,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '이메일을 입력해주세요.';
-                      } else if (value.length < 5) {
-                        return '이메일은 5자 이상이어야 합니다.';
-                      }
-                      return null;
-                    },
+                    errorState: _hasError,
                   ),
                   const SizedBox(height: 10),
                   _buildTextField(
@@ -51,27 +56,21 @@ class _LoginState extends State<Login> {
                     label: '비밀번호',
                     hintText: '비밀번호를 입력해주세요...',
                     obscureText: true,
-                    // 비밀번호 필드에서 문자를 숨김
-                    errorState: _passwordHasError,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '비밀번호를 입력해주세요.';
-                      } else if (value.length < 8) {
-                        return '비밀번호는 8자 이상이어야 합니다.';
-                      }
-                      return null;
-                    },
+                    errorState: _hasError,
                   ),
-                  const SizedBox(height: 5),
-                  _changePasswordBtn(context),
-                  const SizedBox(height: 20),
+                  _changePassWordBtn(),
+                  if (_hasError)
+                    const Text(
+                      '아이디와 비밀번호를 다시 한 번 확인해주세요.',
+                      style: TextStyle(color: Colors.red, fontSize: 14),
+                    ),
                   _loginBtn(context),
                   const SizedBox(height: 10),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       const Text('아직 회원이 아니신가요?'),
-                      _signUpBtn(context),
+                      _signUpBtn(),
                     ],
                   ),
                 ],
@@ -83,14 +82,13 @@ class _LoginState extends State<Login> {
     );
   }
 
-  // 텍스트 필드 위젯을 생성하는 메소드
+  // 텍스트 필드 위젯 생성
   SizedBox _buildTextField({
     required TextEditingController controller,
     required String label,
     required String hintText,
-    bool obscureText = false, // 비밀번호 필드에서 문자를 숨길지 여부
-    required FormFieldValidator<String> validator,
-    required bool errorState,
+    bool obscureText = false,
+    required bool errorState, // 오류 상태 관리
   }) {
     return SizedBox(
       width: 350,
@@ -100,97 +98,74 @@ class _LoginState extends State<Login> {
           Text(
             label,
             style: TextStyle(
-              color:
-                  errorState ? Colors.red : Colors.black, // 오류 상태에 따라 라벨 색상 변경
+              color: errorState ? Colors.red : Colors.black, // 오류 시 라벨 색상 변경
             ),
           ),
           const SizedBox(height: 3),
           TextFormField(
             controller: controller,
             obscureText: obscureText,
-            // 문자를 숨기도록 설정
             decoration: InputDecoration(
-              border: _inputBorder(),
-              enabledBorder: _inputBorder(),
-              focusedBorder: _inputBorder(),
-              errorBorder: OutlineInputBorder(
+              border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(14),
-                borderSide: const BorderSide(color: Colors.red),
+                borderSide: BorderSide(
+                  color: errorState ? Colors.red : const Color(0xFFEFF0F2),
+                ),
               ),
-              focusedErrorBorder: OutlineInputBorder(
+              enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(14),
-                borderSide: const BorderSide(color: Colors.red),
+                borderSide: BorderSide(
+                  color: errorState ? Colors.red : const Color(0xFFEFF0F2),
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: BorderSide(
+                  color: errorState ? Colors.red : const Color(0xFF7E8EF1),
+                ),
               ),
               hintText: hintText,
               hintStyle: TextStyle(
                 color: errorState ? Colors.red : Colors.grey,
-                // 오류 상태에 따라 힌트 텍스트 색상 변경
                 fontSize: 14,
               ),
             ),
             style: TextStyle(
-              color: errorState
-                  ? Colors.red
-                  : Colors.black, // 오류 상태에 따라 입력된 텍스트 색상 변경
+              color: errorState ? Colors.red : Colors.black, // 입력 텍스트 색상 변경
             ),
-            validator: (value) {
-              final result = validator(value);
-              if (result != null) {
-                setState(() {
-                  if (controller == _emailController) {
-                    _emailHasError = true;
-                  } else if (controller == _passwordController) {
-                    _passwordHasError = true;
-                  }
-                });
-              } else {
-                setState(() {
-                  if (controller == _emailController) {
-                    _emailHasError = false;
-                  } else if (controller == _passwordController) {
-                    _passwordHasError = false;
-                  }
-                });
-              }
-              return result;
-            },
           ),
         ],
       ),
     );
   }
 
-  // 공통 입력 경계 스타일
-  OutlineInputBorder _inputBorder() {
-    return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(14),
-      borderSide: const BorderSide(color: Color(0xFFEFF0F2)),
-    );
-  }
-
-  // 회원가입 버튼 위젯을 생성하는 메소드
-  TextButton _signUpBtn(BuildContext context) {
-    return TextButton(
-      onPressed: () {},
-      child: const Text(
-        '회원가입',
-        style: TextStyle(color: Color(0XFF3269F6)),
-      ),
-    );
-  }
-
-  // 로그인 버튼 위젯을 생성하는 메소드
+  // 로그인 버튼
   SizedBox _loginBtn(BuildContext context) {
     return SizedBox(
       width: 350,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor: const Color(0xFF7E8EF1),
+          backgroundColor: ColorData.mainColor,
         ),
         onPressed: () {
-          if (_formKey.currentState?.validate() ?? false) {
-            // 로그인 버튼 클릭 시 수행할 동작 정의
-          }
+          setState(() {
+            // 이메일과 비밀번호 검증
+            if (_emailController.text != _testEmail ||
+                _passwordController.text != _testPassword) {
+              _hasError = true; // 오류 상태 활성화
+            } else {
+              _hasError = false; // 오류 상태 해제
+
+              // 성공적인 로그인 처리
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('로그인 성공!')),
+              );
+
+              // 로그인 성공 시 다음 화면으로 이동 (예시)
+              // Navigator.push(context,
+              //   MaterialPageRoute(builder: (context) => HomePage()));
+            }
+          });
         },
         child: const Text(
           '로그인',
@@ -200,14 +175,36 @@ class _LoginState extends State<Login> {
     );
   }
 
-  // 비밀번호 재설정 버튼 위젯을 생성하는 메소드
-  TextButton _changePasswordBtn(BuildContext context) {
+  // 회원가입 버튼
+  TextButton _signUpBtn() {
     return TextButton(
-      onPressed: () {},
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const SignUp(),
+          ),
+        );
+      },
       child: const Text(
-        '비밀번호 재설정',
-        style: TextStyle(color: Color(0XFF3269F6)),
+        '회원가입',
+        style: TextStyle(
+          color: Color(0XFF3269F6),
+          fontSize: 16,
+        ),
       ),
     );
   }
+}
+
+Widget _changePassWordBtn() {
+  return TextButton(
+    onPressed: () {},
+    child: const Text(
+      '비밀번호 재설정',
+      style: TextStyle(
+        color: Color(0XFF3269F6),
+      ),
+    ),
+  );
 }
