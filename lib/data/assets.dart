5cd09b5666f9410baf1f7087fd3074d790c19b90
nahyunuk <s24022@gsm.hs.kr>
2024-12-12 21:04:06 +0900

    feat: Assets 추가

--- lib/data/assets.dart
+++ lib/data/assets.dart
@@ -1,4 +1,7 @@
 class Assets {
   static const String logo1 = 'assets/images/logo/logo1.svg';
   static const String logo2 = 'assets/images/logo/logo2.svg';
+  static const String search = 'assets/images/search.svg';
+  static const String alarm = 'assets/images/alarm.svg';
+  static const String profile = 'assets/images/profile.svg';
 }
