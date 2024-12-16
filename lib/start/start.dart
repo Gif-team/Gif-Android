811d489580bdc399061152044fa9d2cb428cf001
nahyunuk <s24022@gsm.hs.kr>
2024-12-16 16:50:33 +0900

    feat: 오류 수정

--- lib/start/start.dart
+++ lib/start/start.dart
@@ -11,35 +11,37 @@ class Start extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(Assets.logo1),
-          SizedBox(
-            width: size.width * 0.5,
-            height: 35,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: ColorData.mainColor,
-                surfaceTintColor: ColorData.mainColor,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Login(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(Assets.logo2),
+            SizedBox(
+              width: size.width * 0.5,
+              height: 35,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: ColorData.mainColor,
+                  surfaceTintColor: ColorData.mainColor,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Login(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  '로그인',
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
-                );
-              },
-              child: const Text(
-                '로그인',
-                style: TextStyle(
-                  color: Colors.white,
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
