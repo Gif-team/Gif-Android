37ebcef413f5761b759d0dd6ea929c75122a564b
nahyunuk <s24022@gsm.hs.kr>
2024-12-16 16:47:37 +0900

    feat: assets 추가 및 수정

--- lib/data/assets.dart
+++ lib/data/assets.dart
@@ -1,7 +1,13 @@
 class Assets {
   static const String logo1 = 'assets/images/logo/logo1.svg';
   static const String logo2 = 'assets/images/logo/logo2.svg';
-  static const String search = 'assets/images/search.svg';
-  static const String alarm = 'assets/images/alarm.svg';
-  static const String profile = 'assets/images/profile.svg';
+  static const String search = 'assets/images/appBar/search.svg';
+  static const String alarm = 'assets/images/appBar/alarm.svg';
+  static const String profile = 'assets/images/appBar/profile.svg';
+  static const String add_icon = 'assets/images/floatingActionButton/add_icon.svg';
+  static const String chat_icon = 'assets/images/floatingActionButton/chat_icon.svg';
+  static const String flitter_icon = 'assets/images/floatingActionButton/flitter_icon.svg';
+  static const String badge1 = 'assets/images/badge/Badge1.svg';
+  static const String badge2 = 'assets/images/badge/Badge2.svg';
+  static const String badge3 = 'assets/images/badge/Badge3.svg';
 }
