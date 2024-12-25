8c34cfbe34ae7cd292d7458079de4c7d8339b9a0
nahyunuk <s24022@gsm.hs.kr>
2024-12-26 08:55:17 +0900

    feat: 색상 추가

--- lib/data/colorData.dart
+++ lib/data/colorData.dart
@@ -4,4 +4,6 @@ class ColorData{
   static const Color mainColor =   Color.fromARGB(255, 89, 86, 232);
   static const Color grayColor =   Color(0xFFF4F4F4);
   static const Color postOutLine =   Color(0xFFEFF0F2);
+  static const Color textFieldColor=   Color(0xFFCBCCCE);
+  static const Color dropdownBackgroundColor=   Color(0xFFE9E9E9);
 }
