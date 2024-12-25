c8519afe16e29a040246cc3737e8d28e21f1ab71
nahyunuk <s24022@gsm.hs.kr>
2024-12-26 08:54:46 +0900

    feat: 이미지 asset 추가

--- lib/data/assets.dart
+++ lib/data/assets.dart
@@ -10,4 +10,15 @@ class Assets {
   static const String badge1 = 'assets/images/badge/Badge1.svg';
   static const String badge2 = 'assets/images/badge/Badge2.svg';
   static const String badge3 = 'assets/images/badge/Badge3.svg';
+  static const String camera = 'assets/images/edit/camera.svg';
+  static const String find = 'assets/images/edit/dropDown/find.svg';
+  static const String lost = 'assets/images/edit/dropDown/lost.svg';
+  static const String mainBuilding = 'assets/images/edit/dropDown/main_building.svg';
+  static const String dormitory = 'assets/images/edit/dropDown/dormitory.svg';
+  static const String geumbong = 'assets/images/edit/dropDown/geumbong.svg';
+  static const String f1 = 'assets/images/edit/dropDown/floor/1F.svg';
+  static const String f2 = 'assets/images/edit/dropDown/floor/2F.svg';
+  static const String f3 = 'assets/images/edit/dropDown/floor/3F.svg';
+  static const String f4 = 'assets/images/edit/dropDown/floor/4F.svg';
+  static const String f5 = 'assets/images/edit/dropDown/floor/5F.svg';
 }
